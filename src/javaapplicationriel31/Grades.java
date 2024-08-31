32072211f928a28bf0ca64c859d8075a08309ede
JesrielAlegado <alegadojesriel@gmail.com>
2024-08-31 14:42:23 +0800

    new classes

--- src/javaapplicationriel31/Grades.java
+++ src/javaapplicationriel31/Grades.java
@@ -4,8 +4,8 @@ public class Grades {
     private int id;
     private String name;
     private double p, m, pf, f;
+    double ave;
 
-    // Method to add grades
     public void addGrades(int sid, String sname, double sp, double sm, double spf, double sf) {
         this.id = sid;
         this.name = sname;
@@ -15,13 +15,12 @@ public class Grades {
         this.f = sf;
     }
 
-    // Method to view grades
     public void viewGrades() {
-        double average = (this.p + this.m + this.pf + this.f) / 4; // Average calculation
-        String remarks = (average > 3.0) ? "Failed" : "Passed"; 
+        this.ave = (this.p + this.m + this.pf + this.f) / 4; 
+        String remarks = (ave > 3.0) ? "Failed" : "Passed"; 
 
         System.out.printf("%-10d %-10s %-10.2f %-10.2f %-10.2f %-10.2f %-10.2f %-10s%n",
-                this.id, this.name, this.p, this.m, this.pf, this.f, average, remarks);
+                this.id, this.name, this.p, this.m, this.pf, this.f, ave, remarks);
     }
 
     public static void main(String[] args) {
--- src/javaapplicationriel31/JavaApplicationRiel31.java
+++ src/javaapplicationriel31/JavaApplicationRiel31.java
@@ -3,10 +3,12 @@
 public class JavaApplicationRiel31 {
 
         public static void main(String[] args) {
-             Grades gr = new Grades();
-        gr.addGrades(1011, "Mike Gwapo",1.0 ,2.2,2.3,1.3);
-        gr.viewGrades();
-
+        
+            Product pr = new Product();
+//            pr.getProducts();
+            
+            Grade gr = new Grade();
+            gr.getGrades();
     }
     
 }
